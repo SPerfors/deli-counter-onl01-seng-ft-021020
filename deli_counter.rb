aa22d10e584d9bb72494a0c681120571e113e401
Steven Perfors <stevecanbuild@gmail.com>
2020-02-28 16:41:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(name) 
+  if line(name).length == 0 
+    puts "The line is currently empty."
+  else 
+    line.each_with_index do |name, index|
+      puts "The line is currently: #{index}. #{name}"
+    end
+  end
+end
